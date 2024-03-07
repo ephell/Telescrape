7427d56ad7ace462c9cc93a34a2ef3d71709df2b
ephell <dremaitiserikas@yahoo.com>
2024-03-07 13:48:27 +0200

    Add __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,16 @@
+import sys
+
+from src.gui.application.application import Application
+from src.gui.main_window.main_window import MainWindow
+
+
+def main():
+    app = Application(sys.argv)
+    main_window = MainWindow(app)
+    main_window.show()
+    app.exec()
+
+
+if __name__ == '__main__':
+    main()
+
