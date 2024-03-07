567ec0a8f2d9dcbb3dc1a30ab1ac5284efe6a106
ephell <dremaitiserikas@yahoo.com>
2024-03-07 12:55:45 +0200

    Make instance variables 'protected'; Remove unused imports

--- src/client.py
+++ src/client.py
@@ -1,30 +1,25 @@
-import asyncio
-
 from telethon import TelegramClient
-
-from scraper import Scraper
 
 
 class Client:
 
     def __init__(self, username: str, phone_number: int, api_id: str, api_hash: str):
-        self.username = username
-        self.phone_number = phone_number
-        self.api_id = api_id
-        self.api_hash = api_hash
-        self.client: TelegramClient = None
+        self._username = username
+        self._phone_number = phone_number
+        self._api_id = api_id
+        self._api_hash = api_hash
+        self._client: TelegramClient = None
 
     async def __aenter__(self):
         try:
-            print(f"Signing in as: {self.username} ... ")
-            self.client = TelegramClient("session", self.api_id, self.api_hash)
-            await self.client.start(self.phone_number)
+            print(f"Signing in as: {self._username} ... ")
+            self._client = TelegramClient("session", self._api_id, self._api_hash)
+            await self._client.start(self._phone_number)
             print("Signed in successfully!")
-            return self.client
+            return self._client
         except Exception as e:
             print(f"An error occured while signing in: {e}.")
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.client.disconnect()
+        await self._client.disconnect()
         print("Client exited successfully!")
-
