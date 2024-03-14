db21ffa8db1a95365912019c059979f0ee50e3c8
ephell <dremaitiserikas@yahoo.com>
2024-03-14 11:59:49 +0200

    Add loader.gif

--- src/gui/loading_status_widget/loading_status_widget.py
+++ src/gui/loading_status_widget/loading_status_widget.py
@@ -1,3 +1,5 @@
+from PySide6.QtCore import QByteArray, QTimer, QSize
+from PySide6.QtGui import QMovie
 from PySide6.QtWidgets import QWidget
 
 from src.gui.loading_status_widget.LoadingStatusWidget_ui import Ui_LoadingStatusWidget
@@ -8,7 +10,18 @@ class LoadingStatusWidget(Ui_LoadingStatusWidget, QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        
+        self.setup_loader_gif()
+        self.loader_gif_player.start()
+
+    def setup_loader_gif(self):
+        self.loader_gif_player = QMovie(
+            "src/gui/loading_status_widget/loader.gif", 
+            QByteArray(), 
+            self
+        )
+        self.loader_gif_player.setScaledSize(QSize(30, 30))
+        self.gif_placeholder_label.setMovie(self.loader_gif_player)
+
 
 if __name__ == "__main__":
     import sys
