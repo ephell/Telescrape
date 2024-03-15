f79c4ac648931fe09feed73e7936298223375e5e
ephell <dremaitiserikas@yahoo.com>
2024-03-15 14:04:14 +0200

    Logout from client on Logout button click; Display message boxes where relevant

--- src/gui/main_window/button_logout.py
+++ src/gui/main_window/button_logout.py
@@ -1,5 +1,6 @@
 from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QPushButton
+from PySide6.QtWidgets import QPushButton, QMessageBox
+from qasync import asyncSlot
 
 
 class LogoutButton(QPushButton):
@@ -7,14 +8,28 @@ class LogoutButton(QPushButton):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.client = None
-        # self.clicked.connect(self.on_clicked)
+        self.clicked.connect(self._on_clicked)
+
+    @asyncSlot()
+    async def _on_clicked(self):
+        if self.client is not None:
+            await self.client.logout()
+            QMessageBox(
+                QMessageBox.Information, 
+                "Information", 
+                "Logged out successfully!", 
+                parent=self
+            ).show()
+            self.client = None
+        else:
+            QMessageBox(
+                QMessageBox.Information, 
+                "Information", 
+                "Not logged in.", 
+                parent=self
+            ).show()
 
     @Slot()
-    def on_logged_in_successfully(self, client):
-        print("\nza")
+    def on_client_login_finished(self, client):
         self.client = client
-
-    # @Slot()
-    # def on_clicked(self):
-        
     
