3868e96ef4a078a5d2f9242646296fb964f65ae4
ephell <dremaitiserikas@yahoo.com>
2024-03-22 01:04:00 +0200

    Make central widget and subwidgets 'protected'

--- src/gui/main_window/main_window.py
+++ src/gui/main_window/main_window.py
@@ -17,60 +17,60 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.client: "Client" = None
         self.setWindowTitle("Telescrape")
         self.setFocus()
         self.setFocusPolicy(Qt.ClickFocus) # Make all widgets lose focus when clicking on the main window.
         self.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint) # Set non resizable.
-        self.central_widget = CentralWidget(self)
-        self.setCentralWidget(self.central_widget)
-        self._central_widget_initial_size = self.central_widget.size()
-        self.base_widget = self.get_base_widget()
-        self.overlay_widget = self.get_overlay_widget()
-        self.scrape_widget = self.get_scrape_widget()
+        self._central_widget = CentralWidget(self)
+        self._central_widget_initial_size = self._central_widget.size()
+        self.setCentralWidget(self._central_widget)
+        self._client: "Client" = None
+        self._base_widget = self.get_base_widget()
+        self._overlay_widget = self.get_overlay_widget()
+        self._scrape_widget = self.get_scrape_widget()
         # Signals and slots.
-        self.base_widget.login_button.client_login_finished_signal.connect(self._on_client_login_finished_signal)
-        self.base_widget.login_button.client_login_finished_signal.connect(
-            self.scrape_widget.o_n_client_login_finished_signal
+        self._base_widget.login_button.client_login_finished_signal.connect(self._on_client_login_finished_signal)
+        self._base_widget.login_button.client_login_finished_signal.connect(
+            self._scrape_widget.o_n_client_login_finished_signal
         )
-        self.overlay_widget.login_successful_signal.connect(self._on_login_successful_signal)
-        self.scrape_widget.logout_signal.connect(self._on_logout_signal)
+        self._overlay_widget.login_successful_signal.connect(self._on_login_successful_signal)
+        self._scrape_widget.logout_signal.connect(self._on_logout_signal)
 
 
     def get_base_widget(self) -> "BaseWidget":
-        return self.central_widget.base_widget
+        return self._central_widget.base_widget
 
     def get_overlay_widget(self) -> "OverlayWidget":
-        return self.central_widget.overlay_widget
+        return self._central_widget.overlay_widget
 
     def get_scrape_widget(self) -> "ScrapeWidget":
-        return self.central_widget.scrape_widget
+        return self._central_widget.scrape_widget
 
     @asyncClose
     async def closeEvent(self, event):
         super().closeEvent(event)
         for window in QApplication.topLevelWidgets():
             window.close()
-        if self.client is not None:
-            await self.client.logout()
+        if self._client is not None:
+            await self._client.logout()
 
     @Slot()
     def _on_client_login_finished_signal(self, client: Optional["Client"]):
-        self.client = client
+        self._client = client
 
     @Slot()
     def _on_login_successful_signal(self):
-        self.base_widget.set_hidden(True)
-        self.overlay_widget.set_hidden(True)
-        self.resize(self.central_widget.get_scrape_widget_container_original_size())
-        self.scrape_widget.set_hidden(False)
+        self._base_widget.set_hidden(True)
+        self._overlay_widget.set_hidden(True)
+        self.resize(self._central_widget.get_scrape_widget_container_original_size())
+        self._scrape_widget.set_hidden(False)
 
     @asyncSlot()
     async def _on_logout_signal(self):
-        await self.client.logout()
-        if not self.client.is_connected():
+        await self._client.logout()
+        if not self._client.is_connected():
             print("Logged out successfully!")
         # ToDo: clear the scroll area from all checkboxes.
-        self.scrape_widget.set_hidden(True)
-        self.resize(self.central_widget.get_base_widget_container_original_size())
-        self.base_widget.set_hidden(False)
+        self._scrape_widget.set_hidden(True)
+        self.resize(self._central_widget.get_base_widget_container_original_size())
+        self._base_widget.set_hidden(False)
